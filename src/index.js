affe0fa553acecd5e492c4c289263a5302e9dc08
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-04-27 14:50:11 +0900

    Prepare onsen-ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ons from 'onsenui';
+import { Page, Toolbar, BackButton, ToolbarButton, Icon, Input } from 'react-onsenui';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.min.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      checked: false,
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange() {
+    this.setState({
+      checked: !this.state.checked,
+    })
+  }
+  render() {
+    return (
+      <Page renderToolbar={() =>
+        <Toolbar>
+          <div className="left">
+            <BackButton>Back</BackButton>
+          </div>
+          <div className="center">Title</div>
+          <div className="right">
+            <ToolbarButton>
+              <Icon icon="md-menu" />
+            </ToolbarButton>
+          </div>
+        </Toolbar>}
+      >
+        <Input
+          value={this.state.text} float
+          onChange={(event) => { this.setState({text: event.target.value})} }
+          modifier='material'
+          placeholder='Username' />
+        <Input type="checkbox" checked={this.state.checked} onChange={this.onChange} />
+      </Page>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('react-root')
 );
