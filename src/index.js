7f467796cfe420b8422a6bb6a396683241a53fec
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-05-12 20:33:57 +0900

    onsen

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ons from 'onsenui';
-import {
-  Page,
-  Toolbar,
-  BackButton,
-  ToolbarButton,
-  Icon,
-  Input,
-} from 'react-onsenui';
+import * as Ons from 'react-onsenui';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.min.css';
 
@@ -18,49 +11,57 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      direction: 'to',
       text: '',
       checked: false,
     };
   }
-  onChange = () => {
-    this.setState({
-      checked: !this.state.checked,
-    });
-  };
-  render() {
-    return (
-      <Page
-        renderToolbar={() => (
-          <Toolbar>
-            <div className="left">
-              <BackButton>Back</BackButton>
-            </div>
-            <div className="center">Title</div>
-            <div className="right">
-              <ToolbarButton>
-                <Icon icon="md-menu" />
-              </ToolbarButton>
-            </div>
-          </Toolbar>
-        )}
-      >
-        <Input
-          value={this.state.text}
-          float
-          onChange={event => {
-            this.setState({ text: event.target.value });
-          }}
-          modifier="material"
-          placeholder="Username"
-        />
-        <Input
-          type="checkbox"
-          checked={this.state.checked}
-          onChange={this.onChange}
-        />
-      </Page>
-    );
+  editSelects = (event) => {
+    this.setState({ modifier: event.target.value });
   }
+  render = () => (
+    <Ons.Page>
+      <h1>NAIST Bus Schedule</h1>
+      <Ons.Row>
+        <Ons.Col width="50%" style={{textAlign: 'right'}}>
+          進行方向
+        </Ons.Col>
+        <Ons.Col width="50%">
+          <Ons.Select value={this.state.direction} onChange={e => {
+            this.setState({direction: event.target.value});
+          }}>
+            <option value="to">to NAIST</option>
+            <option value="from">from NAIST</option>
+          </Ons.Select>
+        </Ons.Col>
+      </Ons.Row>
+      <Ons.Row>
+        <Ons.Col width="30%">
+
+        </Ons.Col>
+      </Ons.Row>
+      <Ons.Row>
+        <Ons.Col width="30%">
+
+        </Ons.Col>
+      </Ons.Row>
+      <Ons.Input
+        value={this.state.text}
+        onChange={event => {
+          this.setState({ text: event.target.value });
+        }}
+        float
+        placeholder="Username"
+      />
+      <Ons.Input
+        type="checkbox"
+        checked={this.state.checked}
+        onChange={() => {
+          this.setState({ checked: !this.state.checked })
+        }}
+      />
+    </Ons.Page>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
