0eb4a58e7b117ab45d4c38d748af09fbcdd9c0ef
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-05-12 21:24:15 +0900

    split component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Form = (props: {
+  direction: string,
+  busStop: string,
+  timetable: string,
+  onChange: (string, string) => void,
+}) => (
+  <form className="form-horizontal">
+    <div className="form-group">
+      <label className="col-xs-3 control-label">Direction</label>
+      <div className="col-xs-9">
+        <select
+          className="form-control"
+          value={props.direction}
+          onChange={e => {
+            props.onChange('direction', e.target.value);
+          }}
+        >
+          <option value="to">To NAIST</option>
+          <option value="from">From NAIST</option>
+        </select>
+      </div>
+    </div>
+    <div className="form-group">
+      <label className="col-xs-3 control-label">Bus Stop</label>
+      <div className="col-xs-9">
+        <select
+          className="form-control"
+          value={props.busStop}
+          onChange={e => {
+            props.onChange('busStop', e.target.value);
+          }}
+        >
+          <option value="kitaikoma">Gakken Kita Ikoma</option>
+          <option value="takanohara">Takanohara</option>
+        </select>
+      </div>
+    </div>
+    <div className="form-group">
+      <label className="col-xs-3 control-label">Timetable</label>
+      <div className="col-xs-9">
+        <select
+          value={props.timetable}
+          className="form-control"
+          onChange={e => {
+            props.onChange('timetable', e.target.value);
+          }}
+        >
+          <option value="weekday">for weekday</option>
+          <option value="weekend">for weekend</option>
+        </select>
+      </div>
+    </div>
+  </form>
+);
+
 class App extends React.Component {
   constructor() {
     super();
@@ -20,50 +75,12 @@ class App extends React.Component {
   render = () => (
     <div className="container">
       <h1>NAIST Bus Schedule</h1>
-      <form className="form-horizontal">
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Direction</label>
-          <div className="col-sm-10">
-            <select
-              className="form-control"
-              value={this.state.direction}
-              onChange={e => {
-                this.onChange('direction', e.target.value);
-              }}
-            >
-              <option value="to">To NAIST</option>
-              <option value="from">From NAIST</option>
-            </select>
-          </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Bus Stop</label>
-          <div className="col-sm-10">
-            <select
-              className="form-control"
-              onChange={e => {
-                this.onChange('direction', e.target.value);
-              }}
-            >
-              <option value="kitaikoma">学研北生駒駅</option>
-            </select>
-          </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Timetable</label>
-          <div className="col-sm-10">
-            <select
-              className="form-control"
-              onChange={e => {
-                this.onChange('timetable', e.target.value);
-              }}
-            >
-              <option value="weekday">for weekday</option>
-              <option value="weekend">for weekend</option>
-            </select>
-          </div>
-        </div>
-      </form>
+      <Form
+        direction={this.state.direction}
+        busStop={this.state.busStop}
+        timetable={this.state.timetable}
+        onChange={this.onChange}
+      />
     </div>
   );
 }
