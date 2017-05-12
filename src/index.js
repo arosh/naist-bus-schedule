a63d18a2d97815b5a8105a8b452c802f9c7a8190
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-05-12 21:09:45 +0900

    bootstrap

--- src/index.js
+++ src/index.js
@@ -2,65 +2,69 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ons from 'onsenui';
-import * as Ons from 'react-onsenui';
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.min.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       direction: 'to',
-      text: '',
-      checked: false,
+      busStop: 'kitaikoma',
+      timetable: 'weekday',
     };
   }
-  editSelects = (event) => {
-    this.setState({ modifier: event.target.value });
-  }
+  onChange = (name, value) => {
+    this.setState({
+      [name]: value,
+    });
+  };
   render = () => (
-    <Ons.Page>
+    <div className="container">
       <h1>NAIST Bus Schedule</h1>
-      <Ons.Row>
-        <Ons.Col width="50%" style={{textAlign: 'right'}}>
-          進行方向
-        </Ons.Col>
-        <Ons.Col width="50%">
-          <Ons.Select value={this.state.direction} onChange={e => {
-            this.setState({direction: event.target.value});
-          }}>
-            <option value="to">to NAIST</option>
-            <option value="from">from NAIST</option>
-          </Ons.Select>
-        </Ons.Col>
-      </Ons.Row>
-      <Ons.Row>
-        <Ons.Col width="30%">
-
-        </Ons.Col>
-      </Ons.Row>
-      <Ons.Row>
-        <Ons.Col width="30%">
-
-        </Ons.Col>
-      </Ons.Row>
-      <Ons.Input
-        value={this.state.text}
-        onChange={event => {
-          this.setState({ text: event.target.value });
-        }}
-        float
-        placeholder="Username"
-      />
-      <Ons.Input
-        type="checkbox"
-        checked={this.state.checked}
-        onChange={() => {
-          this.setState({ checked: !this.state.checked })
-        }}
-      />
-    </Ons.Page>
+      <form className="form-horizontal">
+        <div className="form-group">
+          <label className="col-sm-2 control-label">Direction</label>
+          <div className="col-sm-10">
+            <select
+              className="form-control"
+              value={this.state.direction}
+              onChange={e => {
+                this.onChange('direction', e.target.value);
+              }}
+            >
+              <option value="to">To NAIST</option>
+              <option value="from">From NAIST</option>
+            </select>
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="col-sm-2 control-label">Bus Stop</label>
+          <div className="col-sm-10">
+            <select
+              className="form-control"
+              onChange={e => {
+                this.onChange('direction', e.target.value);
+              }}
+            >
+              <option value="kitaikoma">学研北生駒駅</option>
+            </select>
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="col-sm-2 control-label">Timetable</label>
+          <div className="col-sm-10">
+            <select
+              className="form-control"
+              onChange={e => {
+                this.onChange('timetable', e.target.value);
+              }}
+            >
+              <option value="weekday">for weekday</option>
+              <option value="weekend">for weekend</option>
+            </select>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
