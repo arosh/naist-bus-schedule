7aac51bbdc0fbbb64239ce66f223aa6ecb731da3
Sho Iizuka <sho.i518@gmail.com>
2017-05-21 14:38:40 +0900

    style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,30 @@
 import React from 'react';
 
 const styles = {
-  paddingTop: '40px',
+  paddingTop: '20px',
   paddingBottom: '40px',
   textAlign: 'center',
 };
 
 export default () => (
   <div style={styles}>
-    <p>
-      Icons made by
-      {' '}
-      <a href="http://www.freepik.com" title="Freepik">Freepik</a>
-      {' '}
-      from
-      {' '}
-      <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>
-      {' '}
-      is licensed by
-      {' '}
-      <a
-        href="http://creativecommons.org/licenses/by/3.0/"
-        title="Creative Commons BY 3.0"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        CC 3.0 BY
-      </a>
-    </p>
+    Icons made by
+    {' '}
+    <a href="http://www.freepik.com" title="Freepik">Freepik</a>
+    {' '}
+    from
+    {' '}
+    <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>
+    {' '}
+    is licensed by
+    {' '}
+    <a
+      href="http://creativecommons.org/licenses/by/3.0/"
+      title="Creative Commons BY 3.0"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      CC 3.0 BY
+    </a>
   </div>
 );
