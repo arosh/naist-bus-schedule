6480cd599b312bfd8e7eb309e6bce1ea9126ce19
Sho Iizuka <873306+arosh@users.noreply.github.com>
2025-02-23 08:50:47 +0000

    Refactor application state management from MobX to Jotai

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'mobx-react';
-
 import * as serviceWorker from './registerServiceWorker';
 import App from './containers/App';
-import createStores from './stores';
 import './assets/css/bootstrap.min.css';
-
-const stores = createStores();
-
-stores.timeTableStore.updateNow();
-
-setInterval(() => {
-  stores.timeTableStore.updateNow();
-}, 1000);
 
 document.addEventListener('DOMContentLoaded', () => {
   const elem = document.getElementById('react-root');
@@ -22,9 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const root = ReactDOM.createRoot(elem);
     root.render(
       <React.StrictMode>
-        <Provider {...stores}>
-          <App />
-        </Provider>
+        <App />
       </React.StrictMode>
     );
   }
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,12 +1,5 @@
-// @flow
-import {
-  observable,
-  action,
-  computed,
-  autorun,
-  runInAction,
-  makeObservable,
-} from 'mobx';
+import { atom } from 'jotai';
+
 import BusScheduleService from '../services/BusScheduleService';
 import SplitScheduleService from '../services/SplitScheduleService';
 import TimeDiffService from '../services/TimeDiffService';
@@ -29,108 +22,67 @@ export const SCHEDULE = {
   WEEKEND: 'weekend',
 };
 
-export class TimeTableStore {
-  direction: string;
-  busStop: string;
-  scheduleType: string;
-  timeTable: string[] = [];
-  nowHours = 0;
-  nowMinutes = 0;
-  nowSeconds = 0;
-  nextHour: number = 0;
-  nextMinute: number = 0;
-  nextSecond: number = 0;
+const holidayService = new HolidayService();
 
-  busScheduleService = new BusScheduleService();
-  splitScheduleService = new SplitScheduleService();
-  timeDiffService = new TimeDiffService();
+const initialScheduleType =
+  holidayService.checkIfTodayIsHoliday() ||
+  holidayService.checkIfTodayIsWeekend()
+    ? SCHEDULE.WEEKEND
+    : SCHEDULE.WEEKDAY;
 
-  constructor() {
-    this.direction = DIRECTION.FROM_NAIST;
-    this.busStop = BUS_STOP.KITAIKOMA;
-    const holidayService = new HolidayService();
-    this.scheduleType =
-      holidayService.checkIfTodayIsHoliday() ||
-      holidayService.checkIfTodayIsWeekend()
-        ? SCHEDULE.WEEKEND
-        : SCHEDULE.WEEKDAY;
+export const directionAtom = atom(DIRECTION.FROM_NAIST);
+export const busStopAtom = atom(BUS_STOP.KITAIKOMA);
+export const scheduleTypeAtom = atom(initialScheduleType);
 
-    makeObservable(this, {
-      // makeObservable をコンストラクタ内で呼び出す
-      direction: observable,
-      busStop: observable,
-      scheduleType: observable,
-      timeTable: observable,
-      nowHours: observable,
-      nowMinutes: observable,
-      nowSeconds: observable,
-      nextHour: observable,
-      nextMinute: observable,
-      nextSecond: observable,
-      timeTableMap: computed,
-      setDirection: action.bound,
-      setBusStop: action.bound,
-      setScheduleType: action.bound,
-      updateNow: action.bound,
-    });
+const busScheduleService = new BusScheduleService();
 
-    autorun(async () => {
-      // フォームのfrom, toとresourcesのfrom, toを間違えやすいので注意
-      const canonicalDirection = this.direction === 'from' ? 'to' : 'from';
-      // prettier-ignore
-      const query = `${canonicalDirection}-${this.busStop}-${this.scheduleType}`;
-      const timeTable = await this.busScheduleService.fetch(query);
-      runInAction(() => {
-        this.timeTable = timeTable;
-      });
-    });
-    autorun(() => {
-      const diff = this.timeDiffService.getNext(
-        this.nowHours,
-        this.nowMinutes,
-        this.nowSeconds,
-        this.timeTable
-      );
-      runInAction(() => {
-        this.nextHour = diff.hour;
-        this.nextMinute = diff.minute;
-        this.nextSecond = diff.second;
-      });
-    });
-  }
-
-  get timeTableMap(): { [string]: string[] } {
-    return this.splitScheduleService.split(this.timeTable);
-  }
-
-  setDirection = (direction: string) => {
-    this.direction = direction;
-  };
-
-  setBusStop = (busStop: string) => {
-    this.busStop = busStop;
-  };
-
-  setScheduleType = (schedule: string) => {
-    this.scheduleType = schedule;
-  };
-
-  updateNow = () => {
-    const date = new Date();
-    this.nowHours = date.getHours();
-    this.nowMinutes = date.getMinutes();
-    this.nowSeconds = date.getSeconds();
-  };
-}
-
-// Flow に return type を推論させると DRY でうれしいと思いきや，
-// 「any に counterStore が入っている型」に推論されてしまい，全くうれしくない
-export type Stores = {
-  timeTableStore: TimeTableStore,
-};
-
-const createStores: () => Stores = () => ({
-  timeTableStore: new TimeTableStore(),
+const timeTableAtom = atom(async (get) => {
+  const direction = get(directionAtom);
+  const busStop = get(busStopAtom);
+  const scheduleType = get(scheduleTypeAtom);
+  const canonicalDirection = direction === 'from' ? 'to' : 'from';
+  const query = `${canonicalDirection}-${busStop}-${scheduleType}`;
+  return await busScheduleService.fetch(query);
 });
 
-export default createStores;
+const initialDate = new Date();
+
+const rawNowAtom = atom({
+  hours: initialDate.getHours(),
+  minutes: initialDate.getMinutes(),
+  seconds: initialDate.getSeconds(),
+});
+
+export const nowAtom = atom(
+  (get) => {
+    return get(rawNowAtom);
+  },
+  (get, set) => {
+    const date = new Date();
+    set(rawNowAtom, {
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds(),
+    });
+  }
+);
+
+nowAtom.onMount = (setAtom) => {
+  const interval = setInterval(() => {
+    setAtom();
+  }, 1000);
+  return () => clearInterval(interval);
+};
+
+export const nextTimeAtom = atom(async (get) => {
+  const timeDiffService = new TimeDiffService();
+  const { hours, minutes, seconds } = get(nowAtom);
+  const timeTable = await get(timeTableAtom);
+  return timeDiffService.getNext(hours, minutes, seconds, timeTable);
+});
+
+const splitScheduleService = new SplitScheduleService();
+
+export const timeTableMapAtom = atom(async (get) => {
+  return splitScheduleService.split(await get(timeTableAtom));
+});
